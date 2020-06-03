8580d652a133d28a0fab8e50d11fa496c62f0967
ssamkit108 <ssamkit108@gmail.com>
2020-06-03 02:08:33 -0300

    Updated some authentication methods

--- src/main/java/com/dal/catmeclone/authenticationandauthorization/Interface_AuthenticateUserDao.java
+++ src/main/java/com/dal/catmeclone/authenticationandauthorization/Interface_AuthenticateUserDao.java
@@ -1,6 +1,6 @@
 package com.dal.catmeclone.authenticationandauthorization;
 
-import java.sql.SQLException;
+import java.sql.SQLException	;	
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,7 +8,7 @@ import com.dal.catmeclone.exceptionhandler.UserDefinedSQLException;
 import com.dal.catmeclone.model.Course;
 import com.dal.catmeclone.model.User;
 import com.dal.catmeclone.model.UserDetails;
-
+		
 public interface Interface_AuthenticateUserDao {
 	public ArrayList<Course> getallcoursesbyuser(User user) throws SQLException, UserDefinedSQLException;
 
