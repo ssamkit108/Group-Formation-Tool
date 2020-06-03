b69935db2342084375be6b71b89f077844e66e63
ssamkit108 <ssamkit108@gmail.com>
2020-06-02 21:47:05 -0300

    Login authentication and authorisation
    Implemented the Login feature for the catmetool application using spring
    security

---
+++ src/main/java/com/dal/catmeclone/authenticationandauthorization/Interface_AuthenticateUserDao.java
@@ -0,0 +1,22 @@
+package com.dal.catmeclone.authenticationandauthorization;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.dal.catmeclone.exceptionhandler.UserDefinedSQLException;
+import com.dal.catmeclone.model.Course;
+import com.dal.catmeclone.model.User;
+import com.dal.catmeclone.model.UserDetails;
+
+public interface Interface_AuthenticateUserDao {
+	public ArrayList<Course> getallcoursesbyuser(User user) throws SQLException, UserDefinedSQLException;
+
+	public User getbyBannerId(User user) throws SQLException, UserDefinedSQLException;
+
+	public ArrayList<Course> getallcourses()throws SQLException, UserDefinedSQLException;
+
+	public ArrayList<UserDetails> getrolebyuser(User user) throws UserDefinedSQLException, SQLException;
+
+
+}
