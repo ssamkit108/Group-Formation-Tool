8c42d37abae82fcea9608b09d456d0f2a9304e6d
ssamkit108 <ssamkit108@gmail.com>
2020-06-17 19:28:32 -0300

    Fixed Bug in course DAO

--- src/test/java/com/dal/catmeclone/ValidationTest/PasswordRulesLoaderTest.java
+++ src/test/java/com/dal/catmeclone/ValidationTest/PasswordRulesLoaderTest.java
@@ -1,9 +1,19 @@
 package com.dal.catmeclone.ValidationTest;
 
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import com.dal.catmeclone.SystemConfig;
 import com.dal.catmeclone.Validation.PasswordRulesLoader;
 
 public class PasswordRulesLoaderTest {
-
-	private PasswordRulesLoader password;
-	passwordruleloader=SystemConfig.instance();
+	
+	@Test
+	public void TestgetValidationRulesList() {
+		PasswordRulesLoader passwordrules=SystemConfig.instance().getPasswordRules();
+		assertTrue(passwordrules.getValidationRulesList() instanceof List<?>);
+	}
 }
