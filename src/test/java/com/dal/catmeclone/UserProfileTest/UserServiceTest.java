7c665d91644cdd997c3e1b9ad43537d73aabdb8d
ssshah <sm611862@dal.ca>
2020-06-17 22:38:48 -0300

    Updated Testcases for forgotpassword

--- src/test/java/com/dal/catmeclone/UserProfileTest/UserServiceTest.java
+++ src/test/java/com/dal/catmeclone/UserProfileTest/UserServiceTest.java
@@ -1,8 +1,5 @@
 package com.dal.catmeclone.UserProfileTest;
 
-
-
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.util.Assert;
@@ -12,27 +9,21 @@ import com.dal.catmeclone.model.User;
 @SpringBootTest
 class UserServiceTest {
 
-	
-	
 	@SuppressWarnings("deprecation")
 	@Test
 	void CreateTest() {
-		UserValidateMock check=new UserValidateMock();
-
-		User u=new User();
+		UserValidateMock check = new UserValidateMock();
+		UserDaoMock DbMock=new UserDaoMock();
+		User u = new User();
 		u.setBannerId("");
 		Assert.isTrue(!check.validate(u));
-		
-		
-		u=new User();
+		u = new User();
 		u.setBannerId("B00825292");
 		u.setEmail("bob123@gmail.com");
 		u.setFirstName("Bob");
 		u.setLastName("Shaw");
 		u.setPassword("12345");
 		Assert.isTrue(check.validate(u));
-
-		
+		Assert.isTrue(DbMock.createUser(u));
 	}
-
 }
