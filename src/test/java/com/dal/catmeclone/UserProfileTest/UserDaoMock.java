eae5dabb242fec0f275d609939553bbb0bedbed7
rashmika <rashmika.ibrahimpatnam@dal.ca>
2020-06-18 14:07:27 -0700

    adding test cases for UserDao

--- src/test/java/com/dal/catmeclone/UserProfileTest/UserDaoMock.java
+++ src/test/java/com/dal/catmeclone/UserProfileTest/UserDaoMock.java
@@ -5,6 +5,24 @@ import java.util.*;
 import com.dal.catmeclone.model.*;
 
 public class UserDaoMock{
+	String bannerid;
+	String email;
+	String firstname;
+	String lastname;
+	String password;
+	ArrayList<User> userList = new ArrayList<User>();
+
+	public UserDaoMock() {
+		super();
+		
+		this.bannerid = "B00825292";
+		this.email = "bob123@gmail.com";
+		this.firstname = "Bob";
+		this.lastname = "Shaw";
+		this.password= "12345";
+		userList.add(new User(bannerid, email,firstname,lastname,password));
+
+	}
 	public boolean createUser(User user)
 	{	
 		List<User> users=new ArrayList<User>();
@@ -18,4 +36,12 @@ public class UserDaoMock{
 	public boolean UpdatePassword(String username,String password) {
 		return true;
 	}
+	
+	public List<User> getAllUsers() {
+		List<User> l = new ArrayList<User>();
+		l.add(new User("B00825292", "Bob", "Shaw", "12345", "bob123@gmail.com"));
+		return l;
+	}
+
+
 }
