961b10f39e661a36c3241ae14900900b782a15d7
ssshah <sm611862@dal.ca>
2020-07-16 18:13:25 -0300

    Resolved Merge Conflict

---
+++ src/main/java/com/dal/catmeclone/DBUtility/DatabaseConnectionImpl.java
@@ -0,0 +1,74 @@
+package com.dal.catmeclone.DBUtility;
+
+import com.dal.catmeclone.exceptionhandler.UserDefinedException;
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Logger;
+
+public class DatabaseConnectionImpl implements DataBaseConnection {
+
+    private static final Logger LOGGER = Logger.getLogger(DatabaseConnectionImpl.class.getName());
+    private String user;
+    private String password;
+    private String database;
+    private String databaseurl;
+    private String connectionProperty;
+
+    private Connection databaseConnection;
+
+    public DatabaseConnectionImpl() {
+        user = System.getenv("spring.datasource.username");
+        password = System.getenv("spring.datasource.password");
+        database = System.getenv("spring.datasource.name");
+        databaseurl = System.getenv("spring.datasource.url");
+        connectionProperty = System.getenv("datasource.connection.properties");
+    }
+
+
+    @Override
+    public Connection connect() throws UserDefinedException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            LOGGER.severe("Resource Connection Error- Incorrect Driver details");
+            throw new UserDefinedException("Resource Connection Error : Please try again later");
+        }
+
+        try {
+            String databaseConnectionURL = databaseurl + database + "?" + connectionProperty;
+            databaseConnection = DriverManager.getConnection(databaseConnectionURL, user, password);
+        } catch (SQLException e) {
+            LOGGER.severe("DATABASE SERVER ERROR. occured while setting up the database." + e.getLocalizedMessage());
+            throw new UserDefinedException("DATABASE SERVER ERROR. Please try again later");
+        }
+        return databaseConnection;
+    }
+
+    @Override
+    public boolean terminateConnection() throws UserDefinedException {
+        try {
+            if (databaseConnection.isClosed() == false) {
+                databaseConnection.close();
+            }
+        } catch (SQLException e) {
+            LOGGER.severe("DATABASE SERVER ERROR .occured while terminating the connection. " + e.getMessage());
+            throw new UserDefinedException("DATABASE SERVER ERROR. Please try again later.");
+        }
+        return true;
+    }
+
+    @Override
+    public void terminateStatement(CallableStatement statement) throws UserDefinedException {
+        if (statement != null) {
+            try {
+                statement.close();
+            } catch (SQLException e) {
+                LOGGER.severe("DATABASE SERVER ERROR .Occured in Closing Statement " + e.getMessage());
+                throw new UserDefinedException("DATABASE SERVER ERROR");
+            }
+        }
+    }
+}
---
+++ src/main/java/com/dal/catmeclone/exceptionhandler/DuplicateEntityException.java
@@ -0,0 +1,86 @@
+package com.dal.catmeclone.exceptionhandler;
+
+import java.io.PrintStream;
+import java.io.PrintWriter;
+
+public class DuplicateEntityException extends Exception {
+
+    private static final long serialVersionUID = 8461874431183399030L;
+
+    public DuplicateEntityException(String arg0) {
+        super(arg0);
+
+    }
+
+    @Override
+    public synchronized Throwable fillInStackTrace() {
+        return super.fillInStackTrace();
+    }
+
+    @Override
+    public synchronized Throwable getCause() {
+        return super.getCause();
+    }
+
+    @Override
+    public String getLocalizedMessage() {
+        return super.getLocalizedMessage();
+    }
+
+    @Override
+    public String getMessage() {
+        return super.getMessage();
+    }
+
+    @Override
+    public StackTraceElement[] getStackTrace() {
+        return super.getStackTrace();
+    }
+
+    @Override
+    public void setStackTrace(StackTraceElement[] stackTrace) {
+        super.setStackTrace(stackTrace);
+    }
+
+    @Override
+    public synchronized Throwable initCause(Throwable cause) {
+        return super.initCause(cause);
+    }
+
+    @Override
+    public void printStackTrace() {
+        super.printStackTrace();
+    }
+
+    @Override
+    public void printStackTrace(PrintStream s) {
+        super.printStackTrace(s);
+    }
+
+    @Override
+    public void printStackTrace(PrintWriter s) {
+        super.printStackTrace(s);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+}
